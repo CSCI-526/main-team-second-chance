193e54866f0b1b48daed7d583996ee068f1a5912
Timothy Quach <timq720@gmail.com>
2025-03-13 09:06:06 -0700

    Added Black Hole Marble
    Also edited all current Prefabs to have the Marble Tag

---
+++ Assets/Scripts/Marble/Ability/BlackHoleAbility.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBlackHoleAbility", menuName = "ScriptableObjects/Abilities/BlackHole")]
+public class BlackHoleAbility : Ability
+{
+    [SerializeField] private float radius = 1.5f;
+    [SerializeField] private float power = 2.0f;
+    public override void Cast(Marble marble)
+    {
+        Debug.Log("Ability Casted: BLACK HOLE");
+        if (marble == null) return;
+        BlackHole(marble, radius, power);
+    }
+
+    private void BlackHole(Marble marble, float r, float p)
+    {
+        Vector3 castPos = marble.gameObject.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(castPos, radius);
+
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null && hit.CompareTag("Marble") && hit != marble.GetComponent<SphereCollider>())
+            {
+                Vector3 otherPos = rb.gameObject.transform.position;
+
+                Vector3 direction = new Vector3(castPos.x - otherPos.x, castPos.y - otherPos.y, castPos.z - otherPos.z);
+                //rb.AddExplosionForce(-power, explosionPos, radius, 0.0f, ForceMode.Impulse);
+                Debug.Log("ADDED BLACK HOLE FORCE");
+                rb.AddForce(direction * power, ForceMode.Impulse);
+            }
+                
+        }
+    }
+}
