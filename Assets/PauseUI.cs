2528a52b892ebed36c11819becf50b698b898c6f
Timothy Quach <timq720@gmail.com>
2025-04-04 08:04:39 -0700

    Added pause menu and fixed title screen buttons
    Made changes to MainUICanvas prefab

---
+++ Assets/PauseUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    private bool paused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused) Unpause();
+            else Pause();
+            paused = !paused;
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        pausePanel.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
+        SceneManagerScript.Instance.loadSceneByIndex(0);
+    }
+}
