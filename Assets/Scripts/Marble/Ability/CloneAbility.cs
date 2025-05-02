8319f1b29a8f6bcb4fdc37ddde91d492f507db49
Timothy Quach <33167690+tworii@users.noreply.github.com>
2025-05-02 15:07:45 -0700

    CloneAbility Bug fix

--- Assets/Scripts/Marble/Ability/CloneAbility.cs
+++ Assets/Scripts/Marble/Ability/CloneAbility.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewCloneAbility", menuName = "ScriptableObjects/Abilities/Clone")]
 public class CloneAbility : Ability
 {
+
+    public int clones = 0;
     public override void CollisionCast(Marble marble, Marble other)
     {
         Debug.Log("Ability Casted: Clone");
@@ -20,7 +22,17 @@ public class CloneAbility : Ability
         Vector3 Offset = rotation * rb.velocity;
         Vector3 Position = marble.transform.position + 0.1f * Offset.normalized;
 
-        MarbleEvents.MarbleReadyToLaunch(other.Team, other.GetMarbleData(), Offset.normalized, Offset.magnitude, Position, true);
-        AudioManager.TriggerSound(AbilitySound, marble.transform.position);
+        if (clones < 50)
+        {
+            MarbleEvents.MarbleReadyToLaunch(other.Team, other.GetMarbleData(), Offset.normalized, Offset.magnitude, Position, true);
+            AudioManager.TriggerSound(AbilitySound, marble.transform.position);
+            ++clones;
+        }
+    }
+
+    public override float SettledCast(Marble marble)
+    {
+        clones = 0;
+        return 0.0f;
     }
 }
